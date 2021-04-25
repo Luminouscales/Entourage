0f6d85a757f8ac588a4b6d70c4095f38cc9626d9
Luminouscales <loremdrache@gmail.com>
2021-04-25 12:45:19 +0200

    Added functioning skills for Frostlion Guardian

--- garrysmod/gamemodes/entourage/gamemode/ss/ai/guardian.lua
+++ garrysmod/gamemodes/entourage/gamemode/ss/ai/guardian.lua
@@ -1,22 +1,48 @@
 -- This is the script for Frostlion Guardian's battle.
 
+sktbl_guardian = {
+    "Guardian_Bash()",
+    "Guardian_Stagger()"
+}
+
 function GuardianAI()
+    attacktarget = table.Random( allplayers )
+    attacktarget_id = attacktarget:UserID()
     enemy1:UseNoBehavior()
     enemy1:SetNPCState( NPC_STATE_SCRIPT )
     enemy1:ResetSequenceInfo()
+    RunString( table.Random( sktbl_guardian ) )
+end
+
+-- Skills
+function Guardian_Bash()
     enemy1:ResetSequence( "ragdoll" )
     enemy1:ResetSequence( "shove" )
-
     timer.Simple( 0.5, function()
-        KnockerAI()
+        Bash()
     end)
 
     timer.Simple( 1.5, function()
         enemy1:ResetSequence( table.Random( guardian_idle ) )
     end)
     
-    timer.Simple(3, function()
+    timer.Simple(2.5, function()
         playerturn()
     end)
 end
 
+function Guardian_Stagger()
+    enemy1:ResetSequence( "ragdoll" )
+    enemy1:ResetSequence( "bash_cave01" )
+    timer.Simple( 0.8, function()
+        WideStagger()
+    end)
+
+    timer.Simple( 2, function()
+        enemy1:ResetSequence( table.Random( guardian_idle ) )
+    end)
+    
+    timer.Simple( 3, function()
+        playerturn()
+    end)
+end
