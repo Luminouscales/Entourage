925537fdb2da4bb519a5ea155b8194a1d15700ba
Luminouscales <loremdrache@gmail.com>
2021-04-25 12:47:46 +0200

    Fixed Rope Spear

--- garrysmod/gamemodes/entourage/gamemode/ss/weaponfunctions.lua
+++ garrysmod/gamemodes/entourage/gamemode/ss/weaponfunctions.lua
@@ -88,25 +88,24 @@ end
 
 function RopeSpear() -- use pltype2 to derive proper attack from damage type
 	if pltype2 == "Slash" then
-		if math.random( 1, 100 ) <= player:GetNWString( "trueaccNW" ) * ( acc_modifier + slash_acc ) then 
-			wpndmg3 = ( math.random( wpndmg1, wpndmg2 ) + player:GetNWInt("mgtNW") * 0.20 ) * dmg_modifier + slash_dmg
+		if math.random( 1, 100 ) <= pl_stats_tbl[ player:UserID() ].ACC_TRUE * ( acc_modifier + slash_acc ) then 
+			wpndmg3 = ( math.random( wpndmg1, wpndmg2 ) + pl_stats_tbl[ player:UserID() ].MGT * 0.20 ) * dmg_modifier + slash_dmg
 			RunString( enemies_table[ turntargetsave ].AI2 )
 			turntarget:TakeDamage( wpndmg3, player )
 			PrintMessage( HUD_PRINTTALK, player:Name() .." dealt ".. wpndmg3 .." Slash damage to ".. turntargetsave .."!" )
-
 		else
 			PrintMessage( HUD_PRINTTALK, turntargetsave .." dodged ".. player:GetName() .."'s attack!" )
 		end
 	elseif pltype2 == "Pierce" then
 		
-		if math.random( 1, 100 ) <= 5 + player:GetNWInt( "fcsNW" ) * 3 then
+		if math.random( 1, 100 ) <= 5 + pl_stats_tbl[ player:UserID() ].FCS * 3 then
 			critmp = 2.5
 			PrintMessage( HUD_PRINTTALK, player:Name() .." dealt critical damage!" )
 			DoCrit()
 		end
-		if math.random( 1, 100 ) <= player:GetNWString( "trueaccNW" ) * acc_modifier then 
-			wp_pierce = 0.25 + player:GetNWInt( "fcsNW" ) * 0.02
-			wpndmg3 = ( wpndmg1 + player:GetNWInt( "fcsNW" ) * 0.2 ) * critmp * dmg_modifier
+		if math.random( 1, 100 ) <= pl_stats_tbl[ player:UserID() ].ACC_TRUE * acc_modifier then 
+			wp_pierce = 0.25 + pl_stats_tbl[ player:UserID() ].FCS * 0.02
+			wpndmg3 = ( wpndmg1 + pl_stats_tbl[ player:UserID() ].FCS * 0.2 ) * critmp * dmg_modifier
 			RunString( enemies_table[ turntargetsave ].AI2 )
 			turntarget:TakeDamage( wpndmg3, player )
 			PrintMessage( HUD_PRINTTALK, player:Name() .." dealt ".. wpndmg3 .." Pierce damage to ".. turntargetsave .."!" )
@@ -117,8 +116,8 @@ function RopeSpear() -- use pltype2 to derive proper attack from damage type
 		end
 	else -- BLUNT
 		thp = turntarget:Health()
-		if math.random( 1, 100 ) <= player:GetNWString( "trueaccNW" ) * acc_modifier then 
-			wpndmg3 = ( math.random( wpndmg1, wpndmg2 ) + player:GetNWInt( "cstNW" ) * 0.2 ) * dmg_modifier
+		if math.random( 1, 100 ) <= pl_stats_tbl[ player:UserID() ].ACC_TRUE * acc_modifier then 
+			wpndmg3 = ( math.random( wpndmg1, wpndmg2 ) + pl_stats_tbl[ player:UserID() ].VIT * 0.2 ) * dmg_modifier
 			RunString( enemies_table[ turntargetsave ].AI2 )
 			turntarget:TakeDamage( wpndmg3, player )
 			PrintMessage( HUD_PRINTTALK, player:Name() .." dealt ".. wpndmg3 .." Blunt damage to ".. turntargetsave .."!" )
@@ -126,7 +125,7 @@ function RopeSpear() -- use pltype2 to derive proper attack from damage type
 				PrintMessage( HUD_PRINTTALK, player:Name() .." defeated ".. turntargetsave .."!" )
 				deaths = deaths + 1
 			else
-				if math.random( 1, 100 ) <= wpndmg3 / thp * 65 + player:GetNWInt( "mgtNW" ) - enemies_table[ turntargetsave ].DFX then 
+				if math.random( 1, 100 ) <= wpndmg3 / thp * 65 + pl_stats_tbl[ player:UserID() ].MGT - enemies_table[ turntargetsave ].DFX then 
 					turntarget:SetNWInt( "stunturns", turntarget:GetNWInt( "stunturns" ) + 1 )
 					PrintMessage( HUD_PRINTTALK, turntargetsave .." is stunned for ".. turntarget:GetNWInt( "stunturns" ) .." turn(s)..." )
 				end
