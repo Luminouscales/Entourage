089cceaab0df9f9eeaf7c5927a51378888adda1e
Luminouscales <loremdrache@gmail.com>
2021-05-12 20:25:45 +0200

    Added "Ally Call', "Stampede", "Battle Cry", "Wide Stagger" skills; implemented enemy spawning

--- garrysmod/gamemodes/entourage/gamemode/ss/ai/guardian.lua
+++ garrysmod/gamemodes/entourage/gamemode/ss/ai/guardian.lua
@@ -6,12 +6,29 @@ sktbl_guardian = {
 }
 
 function GuardianAI()
+    print("AI is ran.")
     attacktarget = table.Random( allplayers )
     attacktarget_id = attacktarget:UserID()
     enemy1:UseNoBehavior()
     enemy1:SetNPCState( NPC_STATE_SCRIPT )
     enemy1:ResetSequenceInfo()
-    RunString( table.Random( sktbl_guardian ) )
+    -- Call allies every 5 turns
+    if g_stampede ~= true then
+        if g_sudety ~= true then
+            if turn % 10 == 0 then
+                BattleCry()
+            elseif turn % 5 == 0 then -- Do charge attack every 10 turns
+                Guardian_AllyCall()
+            else
+                RunString( table.Random( sktbl_guardian ) )
+            end
+        else
+            Sudety()
+            g_sudety = false
+        end
+    else
+        Stampede()
+    end
 end
 
 -- Skills
@@ -20,6 +37,7 @@ function Guardian_Bash()
 
     enemy1:ResetSequence( "ragdoll" )
     enemy1:ResetSequence( "shove" )
+
     timer.Simple( 0.5, function()
         Bash()
     end)
@@ -29,7 +47,14 @@ function Guardian_Bash()
     end)
     
     timer.Simple(2.5, function()
+        hook.Remove( "EntityTakeDamage", "widestagger" )
         playerturn()
+    end)
+
+    hook.Add( "EntityTakeDamage", "widestagger", function( target, dmg )
+        if target:IsPlayer() and dmg:GetAttacker() == enemy1 then
+            target:EmitSound( "widestagger_2.wav", 150, 100, 1, CHAN_BODY )
+        end
     end)
 end
 
@@ -38,6 +63,10 @@ function Guardian_Stagger()
 
     enemy1:ResetSequence( "ragdoll" )
     enemy1:ResetSequence( "bash_cave01" )
+
+    -- Swing sound
+    enemy1:EmitSound( "widestagger_1.wav", 150, 100, 1, CHAN_BODY )
+
     timer.Simple( 0.8, function()
         WideStagger()
     end)
@@ -47,6 +76,106 @@ function Guardian_Stagger()
     end)
     
     timer.Simple( 3, function()
+        hook.Remove( "EntityTakeDamage", "widestagger" )
+        playerturn()
+    end)
+
+    -- This manages sound appropriately.
+    hook.Add( "EntityTakeDamage", "widestagger", function( target, dmg )
+        if target:IsPlayer() and dmg:GetAttacker() == enemy1 then
+            target:EmitSound( "widestagger_2.wav", 150, 100, 1, CHAN_BODY )
+        end
+    end)
+end
+
+function Guardian_AllyCall()
+    if enemy2 == nil or enemy3 == nil then
+        SendSkillNote( "Ally Call" )
+
+        enemy1:ResetSequence( "ragdoll" )
+        enemy1:ResetSequence( "bark" )
+
+        timer.Simple( 2.75, function()
+            G_AllyCall()
+        end)
+
+        timer.Simple( 2.75, function()
+            enemy1:ResetSequence( table.Random( guardian_idle ) )
+        end)
+        
+        timer.Simple( 5, function()
+            playerturn()
+        end)
+    else
+        -- Refresh if enemies are full
+        RunString( table.Random( sktbl_guardian ) )
+    end
+
+    g_sudety = true
+end
+
+function BattleCry()
+    SendSkillNote( "Battle Cry" )
+
+    enemy1:ResetSequence( "ragdoll" )
+    enemy1:ResetSequence( "roar" )
+
+    timer.Simple( 4, function()
+        enemy1:ResetSequence( table.Random( guardian_idle ) )
+    end)
+    
+    timer.Simple( 5, function()
+        playerturn()
+    end)
+
+    g_stampede = true
+end
+
+function Stampede()
+    SendSkillNote( "Stampede" )
+
+    enemy1:ResetSequence( "ragdoll" )
+    enemy1:ResetSequence( "charge_startfast" )
+
+    timer.Simple( 1, function()
+        enemy1:ResetSequence( "charge_crash" )
+        G_Stampede()
+    end)
+
+    timer.Simple( 4.5, function()
+        enemy1:ResetSequence( table.Random( guardian_idle ) )
+    end)
+    
+    timer.Simple( 6.5, function()
+        hook.Remove( "EntityTakeDamage", "widestagger" )
+        playerturn()
+    end)
+
+    g_stampede = false
+
+    hook.Add( "EntityTakeDamage", "widestagger", function( target, dmg )
+        if target:IsPlayer() and dmg:GetAttacker() == enemy1 then
+            target:EmitSound( "kablamo.wav", 150, 100, 1, CHAN_BODY )
+        end
+    end)
+end
+
+function Sudety()
+    SendSkillNote( "Brute of Sudety" )
+
+    enemy1:ResetSequence( "ragdoll" )
+    enemy1:ResetSequence( "short_roar" )
+
+    timer.Simple( 0.5, function()
+        G_Sudety()
+    end)
+
+    timer.Simple( 2, function()
+        enemy1:ResetSequence( table.Random( guardian_idle ) )
+        SendSkillNote( "Damage resistance decreased!" )
+    end)
+    
+    timer.Simple( 2.5, function()
         playerturn()
     end)
 end
